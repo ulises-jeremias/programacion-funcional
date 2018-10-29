e52abecfffa14ae86ac1736b4ffb30e78cd79640
ulises-jeremias <ulisescf.24@gmail.com>
2018-10-29 09:25:03 -0300

    Updated param names

--- practicas/practica6/practica6.hs
+++ practicas/practica6/practica6.hs
@@ -7,34 +7,34 @@ testTipTree = Join (Join (Join (Join (Tip 1) (Tip 2)) (Tip 3)) (Tip 4)) (Tip 5)
 -- heightTip, longitud del camino más largo desde la raíz hasta una hoja.
 heightTip :: TipTree a -> Int
 heightTip (Tip a) = 0
-heightTip (Join x y) = 1 + (max (heightTip x) (heightTip y))
+heightTip (Join t1 t2) = 1 + (max (heightTip t1) (heightTip t2))
 
 -- leaves, número de hojas.
 leaves :: TipTree a -> Int
 leaves (Tip a) = 1
-leaves (Join x y) = leaves x + leaves y
+leaves (Join t1 t2) = leaves t1 + leaves t2
 
 -- nodes, número de nodos que no son hojas.
 nodes :: TipTree a -> Int
 nodes (Tip a) = 0
-nodes (Join x y) = 1 + nodes x + nodes y
+nodes (Join t1 t2) = 1 + nodes t1 + nodes t2
 
 -- walkover, que devuelve la lista de las hojas de un  árbol, leídas de izquierda a derecha.
 walkover :: TipTree a -> [a]
 walkover (Tip a) = [a]
-walkover (Join x y) = walkover x ++ walkover y
+walkover (Join t1 t2) = walkover t1 ++ walkover t2
 
 -- mirrorTip, que calcula la imagen especular del árbol, o sea, el árbol obtenido 
 -- intercambiando los subárboles izquierdo y derecho de cada nodo.
 mirrorTip :: TipTree a -> TipTree a
 mirrorTip (Tip a) = Tip a
-mirrorTip (Join x y) = Join (mirrorTip y) (mirrorTip x)
+mirrorTip (Join t1 t2) = Join (mirrorTip t2) (mirrorTip t1)
 
 -- mapTip, que toma una función y un árbol, y devuelve el árbol que se obtiene del
 -- dado al aplicar la función a cada nodo.
 mapTip :: (a -> b) -> TipTree a -> TipTree b
 mapTip f (Tip a) = Tip (f a)
-mapTip f (Join x y) = Join (mapTip f x) (mapTip f y)
+mapTip f (Join t1 t2) = Join (mapTip f t1) (mapTip f t2)
 
 -- 4
 
